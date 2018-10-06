e3abad8bd3b18b619c630353852a3c8cc8a3118f
Subhadeep Mohanty <43685701+deepm019@users.noreply.github.com>
2018-10-07 02:16:54 +0530

    Hello.java
    i am contributing source code of hello world in java

---
+++ Hello.java
@@ -0,0 +1,8 @@
+//this is Subhadeep Mohanty,here i donate code using java language to print a hello world in cmd
+//date-7.october.2018
+class Hello
+{
+public static void main(String a[])
+   {
+     System.out.println("HELLO WORLD");
+}}
