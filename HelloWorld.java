f2af0e7fc9e62d0ab6f22f6e0f77ce5dd416fe07
Siddhanta Gadanayak <37920561+siddhantagadanayak@users.noreply.github.com>
2018-10-07 01:40:22 +0530

    Update HelloWorld.java
    Brief explanation of Hello World program in java.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,12 @@
-class HelloWorld
-{
-  public static void main(String[] args)
-  {
-    System.out.println("Hello World!!");
-  }
-}
+class HelloWorld //The first line defines a class called HelloWorld.
+{ // starting of class
+  public static void main(String[] args)//public again means that anyone can access it.
+                                        //static means that you can run this method without creating an instance of Main.
+                                        //void means that this method doesn't return any value.
+                                        //main is the name of the method.
+  { //starting of main function
+    System.out.println("Hello World!!"); //System is a pre-defined class that Java provides us and it holds some useful methods and variables.
+                                        //out is a static variable within System that represents the output of your program (stdout).
+                                        //println is a method of out that can be used to print a line.
+  } // ending of main function.
+} // ending of class
