cf74d708e6f0ef3e08470b58bc008e880eebd187
Nitish Kumar Jha <njha6185@gmail.com>
2018-10-08 01:24:42 +0530

    commented the code

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,8 +1,12 @@
-#include <iostream>
-using namespace std;
+#include <iostream> /* iostream is a header file and include statement is used to add it in code. iostream provides basic input 
+                        and output services for C++ programs. iostream uses the objects cin , cout , cerr ,and clog for sending 
+                        data to and from the standard streams input, output, error (unbuffered),and log (buffered) respectively. */
 
-int main()
+using namespace std;    /* A namespace is a form of scope in C++ that holds its own definitions for variables, functions, etc. 
+                            For example, both cout and cin , along with some useful tokens like endl , are defined inside of std for use.*/
+
+int main()      /* This is main body of code from where execution started*/
 {
-    cout <<"\nHello World"<< endl;
+    cout <<"\nHello World"<< endl; /*Cout is console output it prints everything in double court on the console*/
     return 0;
 }
