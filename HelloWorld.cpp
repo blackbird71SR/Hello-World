e883b68e56d733e925324b1aa4399fbc7d95584a
Saroas Ahamed <isaroasah@gmail.com>
2018-10-14 18:49:43 +0530

    Comments

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 using namespace std;
 
+
 int main()
 {
     cout <<"\nHello World"<< endl;
+    // std::cout <<"\nHello World"<< endl;
+    //if we dont use namespace std pls see the above comment
     return 0;
 }
