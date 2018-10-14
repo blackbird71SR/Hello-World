60ad7930b27e35819c83aafe03c0509f75056728
Saroas Ahamed <isaroasah@gmail.com>
2018-10-14 18:45:49 +0530

    to Keep the console window open till we exit

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -7,6 +7,10 @@ namespace helloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+            //to Keep the console window open till we exit.
+            Console.WriteLine("Click on any key for exit");
+            Console.ReadKey();
         }
     }
 }
