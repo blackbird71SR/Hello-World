8caa9ea5237ed5d89673f6a7f1e7a7d824e8cd30
b2g2102 <35302447+b2g2102@users.noreply.github.com>
2018-10-16 23:12:53 +0530

    this will improve the runtime

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
-
+#define fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
 int main()
 {
+    fast;
     cout <<"\nHello World"<< endl;
     return 0;
 }
