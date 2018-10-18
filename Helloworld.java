7951e6d4d095c83c2289f3bb20d48384699aa7cd
piyumidasanayaka <40532734+piyumidasanayaka@users.noreply.github.com>
2018-10-18 19:38:10 +0530

    Rename HelloWorld.java to Helloworld.java
    Updated Helloworld.java

---
+++ Helloworld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    
+    //by declaring variable
+    String var="Hello World";
+    System.out.println(var);
+    }
+    
+}
