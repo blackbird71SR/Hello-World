81b3eedefbe23570ca79aa4df2d7c9ffbf0f37e1
Fategas <38409508+Fategas@users.noreply.github.com>
2018-10-19 12:06:13 +0100

    cleaned up the HelloWorld.java
    Added a bit of javaDoc standarised comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,13 @@
-class HelloWorld
+public class HelloWorld
 {
-  public static void main(String[] args)
+  //setting up a constructor without any parameters
+  public HelloWorld()
   {
-    System.out.println("Hello World!!");
+    /*
+    * This is a multiline comment
+    * It's here to explain how the printing option of java works
+    * Here, java creates a new line and enters a "hello world!" string into it.
+    */
+    System.out.println("Hello World!");
   }
 }
