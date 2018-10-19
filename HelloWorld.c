d4f10ed15bbbbe0ddf98b47dd0131b1597ec60ce
Sastem <30812913+Sastem@users.noreply.github.com>
2018-10-19 16:27:44 +0200

    Update HelloWorld.c
    without printf !

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,7 +1,21 @@
-#include<stdio.h>
+#include <unistd.h>
 
-int main(void)
+void	ft_putchar(char c)
 {
-  printf("Hello, world!");
-  return 0;
+	write(1, &c, 1);
 }
+
+void	ft_putstr(char *str)
+{
+	while(*str)
+		ft_putchar(*str++);
+}
+
+int	main(void)
+{
+	char str[] = "Hello, World!";
+
+	ft_putstr(str);
+	return (0);
+}
+
