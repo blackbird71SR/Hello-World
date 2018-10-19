a39600cad358ce51236429b35e7348c7a6ce76c5
Denny <denny.kluge@gmail.com>
2018-10-19 22:10:43 +0200

    Update HelloWorld.java
    added some comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,11 @@
 class HelloWorld
 {
+  /**
+  * This is the entry method for the app.
+  */
   public static void main(String[] args)
   {
+    // this prints some text to the standard output
     System.out.println("Hello World!!");
   }
 }
