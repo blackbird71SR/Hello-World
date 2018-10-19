cb4f27e61867841d0631ec20d62f38952509903a
Manish kumar chaurasia <30630740+kmanish31@users.noreply.github.com>
2018-10-19 16:02:06 +0530

    added comments in HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
+#include <iostream>// header file
 using namespace std;
 
 int main()
 {
-    cout <<"\nHello World"<< endl;
+    cout <<"\nHello World"<< endl;// prints Hello World
     return 0;
 }
