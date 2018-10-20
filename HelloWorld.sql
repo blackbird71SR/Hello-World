9adfcdc5e11b0e1593f75bd19960a95a3b4a2809
GenieCode <sharvariauran@gmail.com>
2018-10-20 20:35:37 +0530

    Explained the purpose of different places to print
    Explained the purpose of different places to print as per use case

--- HelloWorld.sql
+++ HelloWorld.sql
@@ -1,2 +1,2 @@
-SELECT 'Hello World';
-PRINT 'Hello World';
+SELECT 'Hello World'; -- This outputs the message
+PRINT 'Hello World'; -- This comes in the message tab along with the output if present 
