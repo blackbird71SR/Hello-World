4eb8f257260149468aa9854d1a1b843d8e63d6c6
krishnendhu <menonkrishnendhu@gmail.com>
2018-10-20 14:04:24 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
-class HelloWorld
-{
-  public static void main(String[] args)
-  {
-    System.out.println("Hello World!!");
-  }
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
 }
