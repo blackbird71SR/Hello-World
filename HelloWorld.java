b986b175654c30481ba390cbf2dd6cf33627e194
KamiBoss116 <42864310+KamiBoss116@users.noreply.github.com>
2018-10-22 23:44:29 +0500

    kamran change print system and give commit in it.

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ class HelloWorld
 {
   public static void main(String[] args)
   {
-    System.out.println("Hello World!!");
+	  //Print hello word using System.out.print command
+    System.out.print("Hello World!!");
   }
 }
