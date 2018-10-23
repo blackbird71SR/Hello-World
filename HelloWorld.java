bb9732521bad515255d8ed9c8d834fd702ce6cf9
Muhammad Usman <itkidunya@outlook.com>
2018-10-23 12:41:48 +0500

    Muhammad Usman

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,4 +4,15 @@ class HelloWorld
   {
     System.out.println("Hello World!!");
   }
+  
+  public static int minFunction(int n1, int n2) 
+  {
+   int min;
+   if (n1 > n2)
+      min = n2;
+   else
+      min = n1;
+
+   return min; 
+  }
 }
