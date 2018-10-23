a170e133541db0beaa04fbc3634caa0fbca43f4c
imran shad <imran.shad@ymail.com>
2018-10-23 05:11:15 -0400

    Hello world using Java GUI Message dialogue

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,9 @@ class HelloWorld
   public static void main(String[] args)
   {
     System.out.println("Hello World!!");
+
+	//Hello world in java using GUI message dialogue	
+	JOptionPane.showMessageDialog(null, "Hello World");
+
   }
 }
