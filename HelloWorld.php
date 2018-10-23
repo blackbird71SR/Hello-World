b6c72d0a6d52223aa5b14ea742fa834af8d7062a
mmursaleen <mee.mursaleen@gmail.com>
2018-10-23 15:00:19 +0500

    add print function

--- HelloWorld.php
+++ HelloWorld.php
@@ -2,6 +2,8 @@
   // In PHP, we use echo to print text
   echo "Hello World";
   // If you want to print in browser's console, we use print_r
+  print("Hello World");
+  // print can also be use in php and it is better than echo 
   print_r("Hello World");
   // if you want the variable data types as well use var_dump
   $stringVar = 'hello world';
