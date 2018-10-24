bd24009907f0741927a6324a718479c4ea2d0f57
Mudit <40176751+mudit458@users.noreply.github.com>
2018-10-25 00:43:13 +0530

    Update HelloWorld.cpp
    Added Explaining Comments

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,6 +1,9 @@
+// Header file for input output functions
 #include <iostream>
 using namespace std;
 
+// main function - 
+// where the execution of program begins
 int main()
 {
     cout <<"\nHello World"<< endl;
