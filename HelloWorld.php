5e714bc2dc70755407d42ff3170278c7d79d33c9
Keven Lefebvre <orditeck@gmail.com>
2018-10-24 13:47:26 -0400

    Update HelloWorld.php
    Remove extra whitespaces, fix incorrect information about print_r, actually remove print_r as it's not what this repo is about IMO, add notes on opening & closing tags

--- HelloWorld.php
+++ HelloWorld.php
@@ -1,9 +1,19 @@
-<?php
-  // In PHP, we use echo to print text
-  echo "Hello World";
-  // If you want to print in browser's console, we use print_r
-  print_r("Hello World");
-  // if you want the variable data types as well use var_dump
-  $stringVar = 'hello world';
-  var_dump($stringVar);
-?>
+<?php // PHP code must be enclosed with <?php tags
+
+echo('Hello World!');
+// Prints Hello World! to stdout.
+// Stdout is the web page if running in a browser.
+
+print('Hello World!'); // The same as echo
+
+// echo and print are language constructs too, so you can drop the parentheses
+echo 'Hello World!';
+print 'Hello World!';
+
+$paragraph = 'paragraph';
+
+echo 100;        // Echo scalar variables directly
+echo $paragraph; // or variables
+
+// If your php file only contains PHP code, it is best practice
+// to omit the php closing tag to prevent accidental output.
