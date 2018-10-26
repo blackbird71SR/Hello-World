102c8f103b9c4561e126476a576268e28141b8b3
nuncharee <nuncharee_fon@hotmail.com>
2018-10-26 21:53:16 +0700

    Hello-Fon

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -2,11 +2,11 @@ using System;
 
 namespace helloWorld
 {
-    class HelloWorld
+    public class HelloWorld
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!");
         }
     }
 }
