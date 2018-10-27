8abcd5c8bdf36d910d173643a3513fc4a5ab7b6b
Sukit Prachprayoon <sukit@kkumail.com>
2018-10-27 17:51:21 +0700

    Hello World  React
    Hello World  React

---
+++ Hello-World.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const Hello = () => {
+  return (<h1>Hello World</h1>)
+}
+
+ReactDOM.render(
+  <Hello />,
+  document.getElementById('app')
+);
