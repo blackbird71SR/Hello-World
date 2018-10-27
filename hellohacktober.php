3e9f8f79cc3f538ebc0bd49c34cb1de1cd73d734
mehulserious99 <44530130+mehulserious99@users.noreply.github.com>
2018-10-27 22:24:45 +0530

    Create hellohacktober.php

---
+++ hellohacktober.php
@@ -0,0 +1,10 @@
+
+<?php
+  // In PHP, we use echo to print text
+  echo "Hello hacktober";
+  // If you want to print in browser's console, we use print_r
+  print_r("Hello Hacktober");
+  // if you want the variable data types as well use var_dump
+  $stringVar = 'hello hacktober';
+  var_dump($stringVar);
+?>
