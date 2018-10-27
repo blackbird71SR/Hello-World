84cc73a3569decc889aea17e18b2bdec29e0455f
RAHUL GOEL <rahul.usit12@gmail.com>
2018-10-27 22:14:28 +0530

    Created HelloWorld in java using frame

---
+++ HelloWorldRahul.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;  //Importing class JFrame
+import javax.swing.JLabel;  //Importing class JLabel
+public class HelloWorldRahul {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();           //Creating frame
+        frame.setTitle("Hi!");                 //Setting title frame
+        frame.add(new JLabel("Hello, world!"));//Adding text to frame
+        frame.pack();                          //Setting size to smallest
+        frame.setLocationRelativeTo(null);     //Centering frame
+        frame.setVisible(true);                //Showing frame
+    }
+}
