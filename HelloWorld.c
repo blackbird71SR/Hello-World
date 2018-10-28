d95f34781d9c8c57ca99d357dd713c8683c4f915
Aakash1822 <30810386+Aakash1822@users.noreply.github.com>
2018-10-28 18:17:35 +0530

    Update HelloWorld.c
    Without main method printing Hello world

--- HelloWorld.c
+++ HelloWorld.c
@@ -5,3 +5,14 @@ int main(void)
   printf("Hello, world!");
   return 0;
 }
+
+
+// Print Hello world without using main method.
+
+#include<stdio.h> 
+#define haxonic main 
+int haxonic(void) 
+{ 
+    printf("Hello, world!"); 
+    return 0; 
+}
