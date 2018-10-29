554bdcbfcbd7e5ca0a7463fbfc73fff6b6df367d
amondrave <32749990+amondrave@users.noreply.github.com>
2018-10-29 07:24:09 -0500

    Update HelloWorld.java
    Imprimir hola mundo usando variables en java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ class HelloWorld
 {
   public static void main(String[] args)
   {
-    System.out.println("Hello World!!");
+    String hola = "Hello world!!";
+    System.out.println(hola);
   }
 }
