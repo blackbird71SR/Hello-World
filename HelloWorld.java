482c19eb2517e08c606a50b875eab0b2f6424edb
Aarav03 <44580992+Aarav03@users.noreply.github.com>
2018-10-29 22:15:08 +0530

    taking input too

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,12 @@
+import java.util.*;
 class HelloWorld
 {
   public static void main(String[] args)
   {
-    System.out.println("Hello World!!");
+    System.out.println("Hello World!!\n");
+    System.out.println("Welcome Here! Enter two numbers.\n");
+    Scanner sc=new Scanner(System.in);
+    String s=sc.nextLine();
+    System.out.println("Thanks for your input. \nYour input-"+s);
   }
 }
