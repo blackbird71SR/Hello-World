d712cffa6bd14f7037db70ff9f57bc8516c29be3
Bartek 'baslow' Słowik <bartek@slowik.cc>
2018-10-29 23:44:36 +0100

    Added hello.bash

---
+++ hello.bash
@@ -0,0 +1,11 @@
+#/bin/bash
+
+RESET="\e[0m"
+BOLD="\e[1m"
+FRED="\e[91m"
+FGREEN="\e[92m"
+FYELLOW="\e[93m"
+FCYAN="\e[96m"
+BGRAY="\e[100m"
+
+echo -e "Hello ${BOLD}${BGRAY}${FRED}b${FGREEN}a${FYELLOW}s${FCYAN}h${RESET}!"
