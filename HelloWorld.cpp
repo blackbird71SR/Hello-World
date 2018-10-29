fe35449b185196d3c11b042e5d8ef0cb784295dd
kikki <kikkidev64@gmail.com>
2018-10-29 14:53:20 +0900

    print helloworld in asciicode

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    cout <<"\nHello World"<< endl;
+    char H = 72;
+    char e = 101;
+    char l = 108;
+    char o = 111;
+    char W = 87;
+    char r = 114;
+    char d = 100;
+    cout << "\n" << H << e << l << l << o << " " << W << o << r << l << d << "\n" << endl;
     return 0;
 }
