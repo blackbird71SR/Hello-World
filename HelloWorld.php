4639254465672e09773d739ab1c383973c428eef
harris <j0h@example.com>
2018-10-29 20:32:21 +0500

    add-h

--- HelloWorld.php
+++ HelloWorld.php
@@ -1,8 +1,8 @@
 <?php
   // In PHP, we use echo to print text
-  echo "Hello World";
+  echo 'HELLO WORLD';
   // If you want to print in browser's console, we use print_r
-  print_r("Hello World");
+  print("Hello World");
   // if you want the variable data types as well use var_dump
   $stringVar = 'hello world';
   var_dump($stringVar);
