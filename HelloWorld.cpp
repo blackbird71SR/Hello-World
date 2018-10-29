d3a5f30b30b8aaf7d30ebbf071d213e8988d55ef
ayushns9 <34533741+ayushns9@users.noreply.github.com>
2018-10-29 11:23:56 +0530

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
+//This program is written in c++ and will print hello world.
+
+#include <iostream> //This is the preprocessor file used in the c++ program.
 using namespace std;
 
-int main()
-{
-    cout <<"\nHello World"<< endl;
-    return 0;
+int main() //This is the makin function.
+{   
+    cout <<"\nHello World"<< endl; //This line prints hello word!
+    return 0; // returns 0 (as the function main is of type int, it is required!)
 }
+  
