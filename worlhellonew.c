5bd6a8976dfe62f2df4e4e9ba5b9475589e5cb91
pushnpull1 <44610387+pushnpull1@users.noreply.github.com>
2018-10-30 19:30:36 +0530

    Update worlhellonew.c

--- worlhellonew.c
+++ worlhellonew.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-   // printf() displays the string inside quotation
+  // printf() displays the string inside quotation
    printf("Hello, World!");
    return 0;
 }
