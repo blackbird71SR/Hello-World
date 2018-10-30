b5213ed2b9f27714d2079f0e01e91e3da40156d1
André Bastos <andrebastos1198@gmail.com>
2018-10-30 18:40:27 -0300

    commit

--- HelloWorld-v2.c
+++ HelloWorld-v2.c
@@ -3,7 +3,8 @@
 int main(){
   char word[] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd'};
 
-  printf("\n"); 
+  printf("\n");
+  printf("\n");
   for(int i = 0; i < 12; i++){
     printf("%c", word[i]);
   }
