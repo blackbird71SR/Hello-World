c436c739e5e9b6c4fabdc90c5837410be1bc1ce1
Maria <maria.clr.dias@gmail.com>
2018-10-30 19:13:15 -0200

    commit

---
+++ helloWorld.java
@@ -0,0 +1,15 @@
+class helloWorld{
+  public static void main(String[] args) {
+    System.out.println("H");
+    System.out.println(" E");
+    System.out.println("  L");
+    System.out.println("   L");
+    System.out.println("    O");
+    System.out.println("     -");
+    System.out.println("      W");
+    System.out.println("       O");
+    System.out.println("        R");
+    System.out.println("         L");
+    System.out.println("          D");
+  }
+}
