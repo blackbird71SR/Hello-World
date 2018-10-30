d8b9bee633f6c1053abb2f00eed581112968601d
botnick <botnick.xxx@gmail.com>
2018-10-30 23:51:07 +0700

    deit HelloWorld.cs

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -6,7 +6,7 @@ namespace helloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World !");
         }
     }
 }
