e6217651b38e0a3441e090973f81969537f74c52
AD2611 <43409904+AD2611@users.noreply.github.com>
2018-10-31 19:50:45 +0530

    Create fast.c

---
+++ fast.c
@@ -0,0 +1,10 @@
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
