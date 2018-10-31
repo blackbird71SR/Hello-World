e82a40b2d71287bc403ff392edb51e252e90f10b
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-10-31 10:13:16 -0400

    Basic demonstration of Hello World using ReactJS

---
+++ HelloWorldReact.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const HelloWorld = () => {
+  return <h1>Hell World, from ReactJs!</h1>
+}
+
+ReactDOM.render(
+  <HelloWorld />, 
+  document.getElementById("root)
+);
