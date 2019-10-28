64a72cb5d52bb5db7d408dfcc8b6b280f4f0f7e2
HasiniUththara <44221630+HasiniUththara@users.noreply.github.com>
2019-10-28 19:42:50 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,9 @@ class HelloWorld
 {
   public static void main(String[] args)
   {
+    //print Hello World!
     System.out.println("Hello World!!");
+    //print What is your name?
+     System.out.println("What is your name?");
   }
 }
