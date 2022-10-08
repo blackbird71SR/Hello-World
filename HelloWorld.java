c618d5857c5fe66cb59dc64a44341f0f87d9f071
fahmy009 <muhammadfahmy009@gmail.com>
2022-10-08 18:28:53 +0700

    update-java hello

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,17 @@ class HelloWorld
   public static void main(String[] args)
   {
     System.out.println("Hello World!!");
+
+    double ItemTransaksi=0;
+
+    for (i=0;i<ItemTransaksi;i++) {
+      System.out.println("ayo pasang transaksi kalian");
+    }
+
+    function hapus(){
+   string nim = "19219893";
+   $model = new m_panitia();
+    $model->delete($nim
+   }
   }
 }
